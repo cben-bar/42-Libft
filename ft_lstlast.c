6c9e322448d29543c2f7ed21ac45cf935d9dc0f2
Clementine Ben barek <cben-bar@z3r4p3.42lyon.fr>
2021-11-17 06:06:21 +0100

    -

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: cben-bar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 03:02:17 by cben-bar          #+#    #+#             */
-/*   Updated: 2021/11/17 05:38:06 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2021/11/17 05:58:49 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_list	*ft_lstlast(t_list *lst)
 	tmp = lst;
 	if (!lst)
 		return (NULL);
-	while (tmp && tmp->next != NULL)
+	while (tmp->next != NULL)
 	{
 		tmp = tmp->next;
 	}
