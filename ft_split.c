86deef33079083fa16aa03bba22d14523855e122
Clementine Ben barek <cben-bar@z4r11p3.42lyon.fr>
2021-11-18 01:16:21 +0100

    free

--- ft_split.c
+++ ft_split.c
@@ -5,80 +5,90 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cben-bar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/15 17:10:10 by cben-bar          #+#    #+#             */
-/*   Updated: 2021/11/16 22:54:09 by cben-bar         ###   ########lyon.fr   */
+/*   Created: 2021/11/18 01:13:11 by cben-bar          #+#    #+#             */
+/*   Updated: 2021/11/18 01:13:17 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strndup(char const *src, size_t n)
+static int	ft_strsuperdup(char **split, int i, const char *s, size_t len)
 {
-	char	*dest;
-	size_t	i;
+	size_t	j;
 
-	dest = malloc(n + 1);
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (src[i] && i < n)
+	j = 0;
+	split[i] = malloc (len + 1);
+	if (!split[i])
 	{
-		dest[i] = src[i];
-		i++;
+		while (i >= 0)
+		{
+			free (split[i]);
+			i--;
+		}
+		free (split);
+		return (0);
 	}
-	dest[i] = 0;
-	return (dest);
+	else
+	{
+		while (s[j] && j < len)
+		{
+			split[i][j] = s[j];
+			j++;
+		}
+		split[i][j] = 0;
+	}
+	return (1);
 }
 
 static size_t	ft_count_words(char const *s, char c)
 {
 	size_t	i;
-	size_t	i_beg;
-	size_t	i_end;
+	size_t	i_b;
+	size_t	i_e;
 
-	i_beg = 0;
-	i_end = 0;
+	i_b = 0;
+	i_e = 0;
 	i = 0;
-	while (s[i_end])
+	while (s[i_e])
 	{
-		if (s[i_end] == c)
+		if (s[i_e] == c)
 		{
-			if (i_end > i_beg)
+			if (i_e > i_b)
 				i++;
-			i_beg = i_end + 1;
+			i_b = i_e + 1;
 		}
-		i_end++;
+		i_e++;
 	}
-	if (i_end > i_beg)
+	if (i_e > i_b)
 		i++;
 	return (i);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char		**string;
-	size_t		i_beg;
-	size_t		i_end;
-	size_t		i;
+	char	**split;
+	size_t	i;
+	size_t	i_b;
+	size_t	i_e;
 
-	string = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
-	if (!string)
-		return (NULL);
-	i_beg = 0;
-	i_end = 0;
+	i_b = 0;
+	i_e = 0;
 	i = 0;
-	while (s[i_end])
+	split = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!split)
+		return (NULL);
+	while (s[i_e])
 	{
-		if (s[i_end] == c)
+		if (s[i_e] == c)
 		{
-			if (i_end > i_beg)
-				string[i++] = ft_strndup (s + i_beg, i_end - i_beg);
-			i_beg = i_end + 1;
+			if (i_e > i_b && !ft_strsuperdup (split, i++, s + i_b, i_e - i_b))
+				return (NULL);
+			i_b = i_e + 1;
 		}
-		i_end++;
+		i_e++;
 	}
-	if (i_end > i_beg)
-		string[i++] = ft_strndup (s + i_beg, i_end - i_beg);
-	string[i] = NULL;
-	return (string);
+	if (i_e > i_b && !ft_strsuperdup (split, i++, s + i_b, i_e - i_b))
+		return (NULL);
+	split[i] = NULL;
+	return (split);
 }
